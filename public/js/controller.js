278cbccf198668fda674127371c84cc15a8be175
GuillermoAlex <guillermo-alexis@hotmail.com>
2016-07-06 22:45:50 -0400

    Modificando el controller

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,36 +1,22 @@
 //Inicializo la variable de mi aplicación.
-var app = angular.module("app", []);
+var myapp = angular.module('appl', []);
 
 //Uso el servicio factory para acceder a socket io y dejarlo como servicio.
-app.factory('socket', function() {
+myapp.factory('socket', function() {
     var socket = io();
     
     return socket;
 });
 
-app.controller("appChat", function ($scope, socket){
-    $scope.apellido = "Gutiérrez";
-    $scope.user_name = "Luis";
-    $scope.mensajes = [];
+myapp.controller("diferenciaCtrlt", function ($scope, socket){
     
-    $scope.enviarMensaje = function(){
-        socket.emit('chat-message', $scope.message);
-        console.log("Envié: "+ $scope.message);
-        $scope.message = "";
-    };
-    
-    socket.on('chat-message', function(msg){
-        console.log("Recibí : "+ msg);
-        $scope.mensajes.push(msg);
-        $scope.$digest();
-    });
-    
-    socket.on('chat-msgs', function(msgs){
-        for(i in msgs){
-            console.log("mensaje: "+msgs[i]);
-            $scope.mensajes.push(msgs[i].mensaje);
-        }
-        $scope.$digest();
-    });
+    $scope.list = [];
+        $scope.text = 'km';
+            $scope.submit = function() {
+                if ($scope.text) {
+                    $scope.list.push(this.text);
+                    $scope.text = '';
+                }
+            };
     
 });
